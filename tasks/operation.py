dcd7d1cb3856463181c3f711c801c46233551951
gwecho <gwecho@qq.com>
2015-11-13 15:36:27 +0800

    Create operation.py

---
+++ tasks/operation.py
@@ -0,0 +1,18 @@
+import os, sys
+from celery_tests import app
+
+@app.task
+def add(x, y):
+    return x + y
+
+@app.task
+def sub(x, y):
+    return x - y
+
+@app.task
+def mul(x, y):
+    return x * y
+
+@app.task
+def div(x, y):
+    return x / y

771c54f966fff74f4d185a21ce995812c0de9aa2
gwecho <gwecho@qq.com>
2015-11-13 15:28:50 +0800

    Create basic_config.py

---
+++ basic_config.py
@@ -0,0 +1,16 @@
+from kombu import Exchange, Queue
+
+##mongo backend config
+CELERY_RESULT_BACKEND = 'mongodb://127.0.0.1:27020/'
+CELERY_MONGODB_BACKEND_SETTINGS = {
+    'database':'celery',
+    'taskmeta_collection': 'my_taskmeta_collection',
+    'max_pool_size':10,
+}
+
+##celery default queue config
+CELERY_DEFAULT_QUEUE = 'celery_default'
+CELERY_DEFAULT_EXCHANGE = 'celery'
+CELERY_DEFAULT_EXCHANGE_TYPE = 'topic'
+CELERY_DEFAULT_ROUTING_KEY = 'task.default'
+
